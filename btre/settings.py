31371460788d7d65d5f54bdd909447d9f4441793
abel <abel.cuza07@gmail.com>
2020-05-31 14:29:28 -0400

    Some views

--- btre/settings.py
+++ btre/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages',
+    'realtors',
+    'listings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btre',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
@@ -123,3 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from realtors.models import Realtor
+from listings.models import Listing
+from listings.choices import bedroom_choices, state_choices, price_choices
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    if request.method == 'GET':
+        listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
+        context = {
+            'listings': listings,
+            'bedroom_choices': bedroom_choices,
+            'state_choices': state_choices,
+            'price_choices': price_choices
+            }
+
+        return render(request, 'pages/index.html', context)
+        
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('id')
+    photos = {'realtors': realtors}
+    return render(request, 'pages/about.html', photos)
