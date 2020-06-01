e41e2501473ca32e85e73f75881789095edd819c
abel <abel.cuza07@gmail.com>
2020-06-01 10:29:27 -0400

    Added dashboard, login and register views and modified the urls.py from the account app

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('login', views.login, name='login'),
     path('register', views.register, name='register'),
-    path('', views.logout, name='logout'),
+    path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
