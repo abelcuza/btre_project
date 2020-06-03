14cb16bb50dae383a6b96dc81c98474f8e66ae4b
abel <abel.cuza07@gmail.com>
2020-06-02 21:11:39 -0400

    Created Contact model. Added the contact app to the settings.py and maked the first mifration

--- btre/settings.py
+++ btre/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'realtors',
     'listings',
     'accounts',
+    'contacts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- contacts/models.py
+++ contacts/models.py
@@ -1,13 +1,18 @@
+from datetime import datetime
+
 from django.db import models
 from listings.models import Listing
+from datetime import datetime
 
 
 class Contact(models.Model):
-    user_id: models.IntegerField()
-    listing: models.ForeignKey(Listing, on_delete=models.DO_NOTHING)
+    listing: models.CharField(max_length=200)
     listing_id: models.IntegerField()
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=50)
     phone = models.CharField(max_length=20)
     message: models.TextField(blank=True)
-    contact_date: models.DateTimeField(datetime.now, blank=True)
+    contact_date: models.DateTimeField(default=datetime.now, blank=True)
+    user_id: models.IntegerField(blank=True)
+    def __str__(self):
+        return self.name
